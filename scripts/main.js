13d0d7f50e95f9e13b6c3d1f8f3a92cf730c6612
ben <30186471+Gammage@users.noreply.github.com>
2026-02-04 14:09:39 +0000

    fixed burger menu

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,7 @@ const menu = document.querySelector('.menu');
 burger.addEventListener('click', () => {
   burger.classList.toggle('active'); // animate burger
   menu.classList.toggle('active');   // show/hide menu
+
+  // toggle scroll
+  document.body.classList.toggle('no-scroll', menu.classList.contains('active'));
 });
